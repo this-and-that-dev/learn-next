d390366dfdd8efafa9df74818918f60c000c5161
공수한 <ksh92315@pionnet.co.kr>
2025-03-04 23:26:55 +0900

    장바구니 스타일 추가

--- components/cart/CartList.jsx
+++ components/cart/CartList.jsx
@@ -1,3 +1,29 @@
-export default function CartList({cart}) {
-    return <div>장바구니 목록</div>
+import Image from 'next/image';
+import styles from './CartList.module.css';
+
+export default function CartList({carts}) {
+
+    return (
+        <>
+            <div>
+                <ul>
+                    {carts.map(cart => {
+                        return (
+                            <li key={cart.id} className={styles.item}>
+                                <div><Image src={cart.imageUrl} width={100} height={100} alt={cart.name}></Image></div>
+                                <div className={styles.description}>
+                                    <div>{cart.name}</div>
+                                    <div>{cart.price}</div>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+            <div>
+                <p>총 가격 : </p>
+                <p>총 수량 : </p>
+            </div>
+        </>
+    )
 }
